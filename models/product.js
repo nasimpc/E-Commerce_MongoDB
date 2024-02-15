76144d43b957f5f219908669ccabe531daa49e50
nasimpc <nasimpcm@gmail.com>
2024-02-15 16:17:47 +0530

    createProduct

--- models/product.js
+++ models/product.js
@@ -1,27 +1,22 @@
-const Sequelize = require('sequelize');
+const getDB = require('../util/database').getDB;
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, imageUrl, description) {
+    this.title = title;
+    this.price = price;
+    this.imageUrl = imageUrl;
+    this.description = description;
   }
-});
+  save() {
+    const db = getDB();
+    return db.collection('products').insertOne(this).then(
+      (res) => {
+        console.log(res);
+      }
+    ).catch((err) => {
+      console.log(err);
+    });
+  }
+}
 
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,8 +1,27 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (callBack) => {
+  MongoClient.connect('mongodb+srv://nasim:kapsuc-mukMik-1dittu@cluster0.9b4haud.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(client => {
+      console.log('Connected');
+      _db = client.db();
+      callBack();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
+};
+const getDB = () => {
+
+  if (_db) {
+    return _db;
+  }
+  throw 'no database found!';
+
+}
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
