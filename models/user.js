a1baff3deef8b3a83ada18342cf5d0b5bef134fa
nasimpc <nasimpcm@gmail.com>
2024-02-17 15:16:05 +0530

    delete_cart_items

--- models/user.js
+++ models/user.js
@@ -16,8 +16,20 @@ class User {
     return db.collection('users').insertOne(this);
   }
 
+  // addToCart(product) {
+  //   const updatedCart = { items: [{ productId: new ObjectId(product._id), quantity: 1 }] };
+  //   const db = getDb();
+  //   console.log(updatedCart);
+  //   return db
+  //     .collection('users')
+  //     .updateOne({ _id: new ObjectId(this._id) },
+  //       { $set: { cart: updatedCart } });
+
+  // }
   addToCart(product) {
+
     const cartProductIndex = this.cart.items.findIndex(cp => {
+
       return cp.productId.toString() === product._id.toString();
     });
     let newQuantity = 1;
@@ -44,6 +56,39 @@ class User {
       );
   }
 
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => {
+      return i.productId;
+    });
+    return db
+      .collection('products')
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then(products => {
+        return products.map(p => {
+          return {
+            ...p,
+            quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          };
+        });
+      });
+  }
+
+  deleteItemFromCart(productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+      return item.productId.toString() !== productId.toString();
+    });
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: { items: updatedCartItems } } }
+      );
+  }
 
   static findById(userId) {
     const db = getDb();
@@ -51,7 +96,6 @@ class User {
       .collection('users')
       .findOne({ _id: new ObjectId(userId) })
       .then(user => {
-        console.log(user);
         return user;
       })
       .catch(err => {
