9c23aeacb24b66eeba6c04257ca4179956be1982
nasimpc <nasimpcm@gmail.com>
2024-02-17 18:12:40 +0530

    relation_mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,23 +18,37 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('65cf32cc80a665ae78dcae19')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('65d0a8dc8b2a604eaa9033d7')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect('mongodb+srv://nasim:kapsuc-mukMik-1dittu@cluster0.9b4haud.mongodb.net/shop?retryWrites=true&w=majority').then(res => {
-  app.listen(3000);
-}).catch(err => {
-  console.log(err);
-});
+mongoose
+  .connect('mongodb+srv://nasim:kapsuc-mukMik-1dittu@cluster0.9b4haud.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'nasim',
+          email: 'nasim@gmail.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
